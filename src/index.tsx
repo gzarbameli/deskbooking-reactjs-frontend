8fbaaee92513f0fd32f591c9d1a7d63ff72597f4
Giacomo Zarba Meli <giacomo.zarba@immobiliare.it>
2023-11-01 11:34:15 +0100

    otel: add web-autoinstrumentation

---
+++ src/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import { Resource } from '@opentelemetry/resources';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import {
+  BatchSpanProcessor,
+  TracerConfig,
+  WebTracerProvider,
+} from '@opentelemetry/sdk-trace-web';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+const collectorOptions = {
+  url: 'http://otel-collector-daemonset-collector.otel-collector.svc.cluster.local/v1/traces', // <-- nginx with reverse proxy to http://localhost:4318/
+};
+
+const providerConfig: TracerConfig = {
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'my-react-app',
+  }),
+};
+
+const provider = new WebTracerProvider(providerConfig);
+
+provider.addSpanProcessor(
+  new BatchSpanProcessor(new OTLPTraceExporter(collectorOptions)),
+);
+
+provider.register({
+  contextManager: new ZoneContextManager(),
+});
+
+registerInstrumentations({
+  instrumentations: [getWebAutoInstrumentations()],
+});
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
